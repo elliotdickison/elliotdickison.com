ea3325667d17d93ff8333acd3ae68018cc203841
Elliot Dickison <ejdickison@gmail.com>
2013-09-21 08:31:43 -0700

    Better menu js.

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,17 @@
-function toggleMenu(){
+function toggleMenu(evt) {
 	var menu = document.getElementById('js-menu');
-	
+
 	menu.className = menu.className == 'show' ? '' : 'show';
+
+	preventDefault(evt);
+}
+
+function preventDefault(evt) {
+    evt = evt || window.event;
+
+    if (evt.preventDefault) {
+        evt.preventDefault();
+    } else {
+        evt.returnValue = false;
+    }
 }
