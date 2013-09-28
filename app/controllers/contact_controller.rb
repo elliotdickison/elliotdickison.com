6602291773329f72f90e6aceec4ed48b3f5b44a0
Elliot Dickison <elliot.dickison@Elliots-MacBook-Pro.local>
2013-09-27 19:41:13 -0700

    Updating pony mail options for production on heroku.

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -21,13 +21,15 @@ post '/contact' do
 	    subject: params[:name] << " via elliotjam.es",
 	    body: params[:body],
 	    :via => :smtp,
-	    :via_options => {
-	      :address => 'smtp.sendgrid.net',
-	      :port => 25,
-	      :authentication => :plain,
-	      :user_name => ENV['SENDGRID_USERNAME'],
-	      :password => ENV['SENDGRID_PASSWORD'],
-	      :domain => ENV['SENDGRID_DOMAIN'] }
+		  :via_options => {
+		    :address => 'smtp.sendgrid.net',
+		    :port => '587',
+		    :domain => 'heroku.com',
+		    :user_name => ENV['SENDGRID_USERNAME'],
+		    :password => ENV['SENDGRID_PASSWORD'],
+		    :authentication => :plain,
+		    :enable_starttls_auto => true
+		  }
 	  })
 	end
 
