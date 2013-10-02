0cfc2c50b482a508f0fbd97e852293f924c27829
Elliot Dickison <ejdickison@gmail.com>
2013-10-02 08:11:51 -0700

    Added post publishing feature. Started 404 page.

---
+++ db/migrate/20131001151726_posts_add_published_at_column.rb
@@ -0,0 +1,9 @@
+class PostsAddPublishedAtColumn < ActiveRecord::Migration
+  def up
+  	add_column :posts, :published_at, :timestamp
+  end
+
+  def down
+  	remove_column :posts, :published_at
+  end
+end
