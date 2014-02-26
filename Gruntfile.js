dfc734b0d829161e06f770babca84574591cd52d
Elliot Dickison <ejdickison@gmail.com>
2014-02-26 12:46:20 -0800

    A bit of Gruntfile cleanup.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,26 +2,30 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
+    
     pkg: grunt.file.readJSON('package.json'),
+
     uglify: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
       },
       build: {
         src: 'public/js/app.js',
-        dest: 'public/build/app.min.js'
-      }
+        dest: 'public/build/app.min.js',
+      },
     },
+
     sass: {
       options: {
-        cacheLocation: 'tmp/sass_cache'
+        cacheLocation: 'tmp/sass_cache',
       },
       dist: {
         files: {
-          'public/css/app.css' : 'public/css/app.scss'
-        }
-      }
+          'public/css/app.css' : 'public/css/app.scss',
+        },
+      },
     },
+
     jshint: {
       options: {
         curly: true,
@@ -29,35 +33,38 @@ module.exports = function(grunt) {
         browser: true,
         smarttabs: true,
         globals: {
-          jQuery: true
+          jQuery: true,
         },
       },
-      uses_defaults: ['public/js/app.js']
+      uses_defaults: ['public/js/app.js'],
     },
+
     command: {
       run_cmd: {
-        cmd: 'touch tmp/restart.txt'
-      }
+        cmd: 'touch tmp/restart.txt',
+      },
     },
+
     watch: {
       css: {
         files: 'public/css/*.scss',
-        tasks: ['sass']
+        tasks: ['sass'],
       },
       js: {
         options: {
           spawn: false,
         },
         files: 'public/js/*.js',
-        tasks: ['jshint', 'uglify']
+        tasks: ['jshint', 'uglify'],
       },
       app: {
         files: ['**/*.rb', 'Gemfile'],
-        tasks: ['command']
-      }
-    }
-  });
+        tasks: ['command'],
+      },
+    },
 
+  });
+  
   grunt.loadNpmTasks('grunt-contrib-jshint'); // JSHint!
   grunt.loadNpmTasks('grunt-contrib-uglify'); // Minify schtuff
   grunt.loadNpmTasks('grunt-contrib-sass'); // CSS with superpowers
